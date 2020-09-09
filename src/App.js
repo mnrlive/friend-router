ceb1f3e02620c19e88dcd9308245b2accac39d6f
Madhu Babu <17622671+Pandimadhubabu@users.noreply.github.com>
2020-09-09 15:13:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import { useEffect } from 'react';
 import Home from './components/Home/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NoMatch from './NoMatch/NoMatch';
